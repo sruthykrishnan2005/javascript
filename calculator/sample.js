9c5b073f5403ae67287e00bf765f5865a3e8ff10
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-09-19 16:40:40 +0530

    javascripts

---
+++ calculator/sample.js
@@ -0,0 +1,25 @@
+function sample1(){
+    a=parseInt(document.getElementById("fno").value)
+    b=parseInt(document.getElementById("sno").value) 
+    document.getElementById("h1").innerHTML=a+b
+}
+function sample2(){
+    a=parseInt(document.getElementById("fno").value)
+    b=parseInt(document.getElementById("sno").value) 
+    document.getElementById("h1").innerHTML=a-b
+}
+function sample3(){
+    a=parseInt(document.getElementById("fno").value)
+    b=parseInt(document.getElementById("sno").value) 
+    document.getElementById("h1").innerHTML=a*b
+}
+function sample4(){
+    a=parseInt(document.getElementById("fno").value)
+    b=parseInt(document.getElementById("sno").value) 
+    document.getElementById("h1").innerHTML=a/b
+}
+function sample5(){
+    a=parseInt(document.getElementById("fno").value)
+    b=parseInt(document.getElementById("sno").value) 
+    document.getElementById("h1").innerHTML=a%b
+}
--- sample.js
+++ sample.js
@@ -17,46 +17,6 @@
 //     alert(a+b)
 // }
 
-// function sample1(){
-//     a=parseInt(document.getElementById("fno").value)
-//     b=parseInt(document.getElementById("sno").value) 
-//     document.getElementById("h1").innerHTML=a+b
-// }
-// function sample2(){
-//     a=parseInt(document.getElementById("fno").value)
-//     b=parseInt(document.getElementById("sno").value) 
-//     document.getElementById("h1").innerHTML=a-b
-// }
-// function sample3(){
-//     a=parseInt(document.getElementById("fno").value)
-//     b=parseInt(document.getElementById("sno").value) 
-//     document.getElementById("h1").innerHTML=a*b
-// }
-// function sample4(){
-//     a=parseInt(document.getElementById("fno").value)
-//     b=parseInt(document.getElementById("sno").value) 
-//     document.getElementById("h1").innerHTML=a/b
-// }
-// function sample5(){
-//     a=parseInt(document.getElementById("fno").value)
-//     b=parseInt(document.getElementById("sno").value) 
-//     document.getElementById("h1").innerHTML=a%b
-// }
-
-
-
-// function sample1(){
-//     a=parseInt(document.getElementById("fno").value)
-//     b=parseInt(document.getElementById("sno").value) 
-//     if(a>b){
-//         document.getElementById("h1").innerHTML=a
-//     }
-//     else{
-//         document.getElementById("h1").innerHTML=b
-//     }
-// }
-
-
 
 
 
