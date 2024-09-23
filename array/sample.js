c3f038eb43dc7162253b730317c35846e6608b5f
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-09-23 11:34:07 +0530

    javascriptadded

---
+++ array/sample.js
@@ -0,0 +1,55 @@
+l=[1,2,3,4,5,6]
+
+//.......array length//
+
+// console.log(typeof l);
+// console.log(l[0]);
+// console.log(l[3]);
+// console.log(l.length);
+// a=l.toString()
+// console.log(typeof a);
+// l.pop()
+// console.log(l);
+// l.push(15)
+// console.log(l);
+
+
+// l[10]=100
+// console.log(l);
+// console.log(l.length);
+// l.shift()
+// console.log(l);
+// l.unshift(101)
+// console.log(l[0]);
+// delete l[0]
+// console.log(l);
+
+
+// //....iterateloop//
+
+// //......for in
+// for(let i in l){
+//     console.log(i)
+// }
+
+// //.....for of
+// for(let i of l){
+//     console.log(i)
+// }
+
+
+// //....checkvalue//
+
+// for(let i of l){
+//     if(i==5){
+//         console.log(i);
+//     }
+// }
+
+// //....use break//
+// for(let i of l){
+//     console.log(i);
+//     if(i==5){
+//         break
+//     }
+// }
